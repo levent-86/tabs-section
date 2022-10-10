d9f87ce2b0ed753cb772fd49e9cc66784c4ca3db
levent-86 <101482776+levent-86@users.noreply.github.com>
2022-10-10 23:16:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,30 +17,3 @@ function btnUpdate() {
         writing.classList.remove("activate");
     });
 }
-
-/* 
-senior'ın çözümü: DOM içerisindekileri function içine alırsan, DOM daha okunaklı hale gelir dedi.
-
-const btns = document.querySelectorAll(".btn");
-const writings = document.querySelectorAll(".writings");
-
-btns.forEach((btn, index) => btn.addEventListener(
-  "click", () => {
-    activateButton(btn);
-    activateWriting(index);
-  }));
-
-function activateButton(btn) {
-  btns.forEach(btn => {
-        btn.classList.remove("active");
-  });
-  btn.classList.add("active");
-}
-
-function activateWriting(index) {
-  writings.forEach(writing => {
-      writing.classList.remove("activate");
-   });
-  writings[index].classList.add("activate");
-}
-*/
